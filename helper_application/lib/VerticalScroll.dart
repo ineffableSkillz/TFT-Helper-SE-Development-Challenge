1b4ae17d8f0bcf5ba7e0a6167c4d66c1a40ff623
Dante Gray (PGR) <b9054070@newcastle.ac.uk>
2020-03-12 14:52:35 +0000

    Scrollable List View Implemented

---
+++ helper_application/lib/VerticalScroll.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+
+List<String> championNames = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe"];
+List<Image> championImages;
+
+void main() {
+  runApp(MyApp());
+}
+
+String  _getChampionImagePath(String champName) {
+  return 'assets/images/playerIcons/' + champName + 'Square.png';
+
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Scaffold(
+        body: new verticalScrollViewWidget(),
+    ),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class verticalScrollViewWidget extends StatefulWidget {
+  @override
+  _verticalScrollViewWidgetState createState() => _verticalScrollViewWidgetState();
+
+}
+// ignore: camel_case_types
+class _verticalScrollViewWidgetState extends State<verticalScrollViewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+        itemBuilder: (context, pos) {
+          return Card(
+            child: InkWell(
+
+              splashColor: Colors.black,
+              onTap: () {
+
+              },
+              child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ListTile(
+                    leading: Image(image: AssetImage(_getChampionImagePath(championNames[pos]))),
+                    title: Text(championNames[pos], style: TextStyle(fontSize: 22.0),),
+                  )
+              ),
+            )
+          );
+        },
+        itemCount: championNames.length,
+        physics: BouncingScrollPhysics(),
+    );
+  }
+}
